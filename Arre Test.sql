c1eb1871be1e9a127f236c7b256bddee38efc1e6
Aravind P V <araprak11@gmail.com>
2023-01-21 14:55:43 +0530

    Add files via upload

---
+++ Arre Test.sql
@@ -0,0 +1,3 @@
+-- We first create a SQL database to store the whatsapp group names and their descriptions to ensure there is no overlap in the group titles.
+-- We then extract the phone numbers who are present in the whatsapp groups.
+-- We extract the messages present in the whatapp groups and sort them accroding to the phone numbers they were sent from to ensure there is no disparity in the text messages.
